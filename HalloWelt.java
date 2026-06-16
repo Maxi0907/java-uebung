409217cfbbffb7d2df37b61e83bac319e8bf77a1
Maxi <maximayer@outlook.com>
2026-06-16 14:50:01 +0200

    Ausgabe Name & Datum

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,6 +1,10 @@
+import java.time.LocalDate;
+
 public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub!");
+        System.out.println("Name: " + System.getProperty("user.name"));
+        System.out.println("Datum: " + LocalDate.now());
     }
 }
 
