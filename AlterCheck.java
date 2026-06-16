ce5d85cd37879686baf2f143ee7c88b1de7832af
Maxi <maximayer@outlook.com>
2026-06-16 15:09:47 +0200

    AlterCheck erweitert: Name, drei Stufen und Fehlermeldung

--- AlterCheck.java
+++ AlterCheck.java
@@ -2,17 +2,24 @@ import javax.swing.JOptionPane;
 
 public class AlterCheck {
     public static void main(String[] args) {
-        // TODO 1: Frage per Popup nach dem Alter und merke dir die Eingabe
+        String name = JOptionPane.showInputDialog("Wie heißt du?");
         String eingabe = JOptionPane.showInputDialog("Wie alt bist du?");
 
-        // TODO 2: Wandle die Eingabe in eine ganze Zahl um
-        int alter = Integer.parseInt(eingabe);
+        try {
+            int alter = Integer.parseInt(eingabe);
+            String nachricht;
 
-        // TODO 3: Bedingung -> ab 18 'volljaehrig', sonst 'minderjaehrig'
-        if (alter >= 18) {
-            JOptionPane.showMessageDialog(null, "Du bist volljaehrig!");
-        } else {
-            JOptionPane.showMessageDialog(null, "Du bist noch minderjaehrig.");
+            if (alter < 13) {
+                nachricht = "Hallo " + name + ", du bist ein Kind.";
+            } else if (alter < 18) {
+                nachricht = "Hallo " + name + ", du bist ein Jugendlicher.";
+            } else {
+                nachricht = "Hallo " + name + ", du bist volljaehrig!";
+            }
+
+            JOptionPane.showMessageDialog(null, nachricht);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Bitte gib eine ganze Zahl für dein Alter ein.");
         }
     }
 }
